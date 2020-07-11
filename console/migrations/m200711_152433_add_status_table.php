81fdb3a0fea7069fc7d02736194cc7797e5834a2
Roma Kulay <romakulay@me.com>
2020-07-11 21:50:07 +0500

    fix

--- console/migrations/m200711_152433_add_status_table.php
+++ console/migrations/m200711_152433_add_status_table.php
@@ -13,20 +13,20 @@ class m200711_152433_add_status_table extends Migration
      */
     public function safeUp()
     {
-//        $tableOptions = null;
-//        if ($this->db->driverName === 'mysql') {
-//            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-//            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-//        }
-//
-//        $this->createTable('{{%status}}', [
-//            'id' => $this->primaryKey(),
-//            'code' => $this->string()->notNull()->unique(),
-//            'name' => $this->string(),
-//            'active' => $this->boolean()->defaultValue(0),
-//            'available' => $this->boolean()->defaultValue(0),
-//            'ordering' => $this->integer()->defaultValue(0),
-//        ], $tableOptions);
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%status}}', [
+            'id' => $this->primaryKey(),
+            'code' => $this->string()->notNull()->unique(),
+            'name' => $this->string(),
+            'active' => $this->boolean()->defaultValue(0),
+            'available' => $this->boolean()->defaultValue(0),
+            'ordering' => $this->integer()->defaultValue(0),
+        ], $tableOptions);
 
         SettingsModel::deleteAll("`key` LIKE '" . SettingsModel::PARAM_CRM_STATUS_LIST . "'");
     }
